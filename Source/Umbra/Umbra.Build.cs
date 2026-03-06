5de3e8b85ec2c390f6da1d91c4c9ee05ed029550
tonytgrt <tonytg@seas.upenn.edu>
2026-03-05 19:51:23 -0500

    unreal template

---
+++ Source/Umbra/Umbra.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Umbra : ModuleRules
+{
+	public Umbra(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Umbra",
+			"Umbra/Variant_Strategy",
+			"Umbra/Variant_Strategy/UI",
+			"Umbra/Variant_TwinStick",
+			"Umbra/Variant_TwinStick/AI",
+			"Umbra/Variant_TwinStick/Gameplay",
+			"Umbra/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Umbra/Umbra.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Umbra.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Umbra, "Umbra" );
+
+DEFINE_LOG_CATEGORY(LogUmbra)
+ 
